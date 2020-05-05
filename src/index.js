573cb2690a461b072d48d0655a74e6ec980863aa
Daniel Richards <drichards211@yahoo.com>
2020-05-04 21:30:15 -0400

    Add completed exercise #2c

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
@@ -17,6 +16,12 @@ function ErrorBox({ children }) {
     </div>
   );
 }
+
+let childOne = 'Mary';
+let childTwo = 'Potato';
+let childThree = 'John';
+let childFour = 'Edward';
+let childFive = 'Jacob';
 
 function FirstChildOnly({ children }) {
   let firstChild = React.Children.toArray(children).slice(0, 1); /* converts
@@ -65,27 +70,58 @@ function Tail({ children, number }) {
   );
 }
 
-let childOne = 'Mary';
-let childTwo = 'Potato';
-let childThree = 'John';
-let childFour = 'Edward';
-let childFive = 'Jacob';
+function Head({ children, number }) {
+  let nChildren = React.Children.toArray(children).slice(0, number);
+  let plural = number > 1;
+  return (
+    <div>
+      The first {plural ? `${number} children are:`: 'child is:'} {nChildren}
+    </div>
+  );
+}
 
 function RenderAll() {
   return (
     <div>
+      <h2>Exercise #1 - Make a component to display an "error box."</h2>
       <ErrorBox>
         Something catastrophic occurred!
       </ErrorBox>
-      <br/><br/>
+      <br/>
+      <h2>Exercise #2 - Practice using the React.Children.toArray function with 
+        these next few exercises.</h2>
+      <div>Children list: 
+        <div>(1) {childOne}</div>
+        <div>(2) {childTwo}</div>
+        <div>(3) {childThree}</div>
+        <div>(4) {childFour}</div>
+        <div>(5) {childFive}</div>
+      </div>
+      <br/>
+      <h2>Exercise #2a - Write a component called FirstChildOnly to accept any 
+        number of children but only render the first.</h2>
       <FirstChildOnly>
         {childOne}{childTwo}{childThree}{childFour}{childFive}
       </FirstChildOnly>
       <br/>
+      <h2>Exercise #2b - Write a component called LastChildOnly that only 
+        renders its last child.</h2>
       <LastChildOnly>
         {childOne}{childTwo}{childThree}{childFour}{childFive}
       </LastChildOnly>
       <br/>
+      <h2>Exercise #2c - Create a component named Head that takes a number prop,
+        and renders the first [number] children.</h2>
+      <Head number={2}>
+        <div>{childOne}</div>
+        <div>{childTwo}</div>
+        <div>{childThree}</div>
+        <div>{childFour}</div>
+        <div>{childFive}</div>
+      </Head>
+      <br/>
+      <h2>Exercise #2d - Create a component named Tail that takes a number and 
+        renders the last N children.</h2>
       <Tail number={3}>
         {childOne}{childTwo}{childThree}{childFour}{childFive}
       </Tail>
@@ -93,5 +129,4 @@ function RenderAll() {
   );
 }
 
-ReactDOM.render(<RenderAll/>, document.querySelector('#root')
-);
+ReactDOM.render(<RenderAll/>, document.querySelector('#root'));
